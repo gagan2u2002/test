586ffdc2ae1c26c4730518bb24508dcb78b2bd01
GAGAN TAYGI <gagan2u2002@gmail.com>
2020-01-25 00:02:03 +0530

    first commit

---
+++ src/main/java/com/etechon/test/App.java
@@ -0,0 +1,13 @@
+package com.etechon.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
